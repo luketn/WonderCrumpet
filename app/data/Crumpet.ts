37089caa2777114ed2b350aa2468413daf2bbd1d
lthompson <lthompson@infomedia.com.au>
2016-06-18 20:23:41 +1000

    Define crumpet data. Note the patch for a lack of string based enums to be fixed in a future version of TypeScript.

---
+++ app/data/Crumpet.ts
@@ -0,0 +1,13 @@
+
+type CrumpetType = "English" | "Scottish" | "Square" | "Pikelet";
+export const CrumpetTypes = {
+    English: "English" as CrumpetType,
+    Scottish: "Scottish" as CrumpetType,
+    Square: "Square" as CrumpetType,
+    Pikelet: "Pikelet" as CrumpetType
+};
+
+export interface Crumpet {
+    name:string;
+    type:CrumpetType;
+}
